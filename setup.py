6049e95239d4b7fb0e9faba70ec18d8ac92f1107
Benoit Suttor <bsuttor@imio.be>
2019-11-25 10:48:26 +0100

    Make works with py3

--- setup.py
+++ setup.py
@@ -5,16 +5,18 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-long_description = '\n\n'.join([
-    open('README.rst').read(),
-    open('CONTRIBUTORS.rst').read(),
-    open('CHANGES.rst').read(),
-])
+long_description = "\n\n".join(
+    [
+        open("README.rst").read(),
+        open("CONTRIBUTORS.rst").read(),
+        open("CHANGES.rst").read(),
+    ]
+)
 
 
 setup(
-    name='imioweb.policy',
-    version='1.0b2.dev0',
+    name="imioweb.policy",
+    version="1.0b2.dev0",
     description="Policy for the installation of buildout.imioweb",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
@@ -28,47 +30,44 @@ setup(
         "Operating System :: OS Independent",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
     ],
-    keywords='Python Plone',
-    author='Nicolas Demonte',
-    author_email='support@imio.be',
-    url='https://pypi.python.org/pypi/imioweb.policy',
-    license='GPL version 2',
-    packages=find_packages('src', exclude=['ez_setup']),
-    namespace_packages=['imioweb'],
-    package_dir={'': 'src'},
+    keywords="Python Plone",
+    author="Nicolas Demonte",
+    author_email="support@imio.be",
+    url="https://pypi.python.org/pypi/imioweb.policy",
+    license="GPL version 2",
+    packages=find_packages("src", exclude=["ez_setup"]),
+    namespace_packages=["imioweb"],
+    package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
     install_requires=[
         # -*- Extra requirements: -*-
-        'plone.api>=1.8.3',
-        'Products.GenericSetup>=1.8.2',
-        'setuptools',
-        'z3c.jbot',
-        'eea.facetednavigation',
-        'collective.faceted.taxonomywidget',
-        'collective.taxonomy',
-        'plone.app.imagecropping',
-        'plone.app.mosaic',
-        'imioweb.theme',
-        'collective.behavior.banner',
-        'collective.behavior.gallery',
-        'collective.easyform',
-        'collective.preventactions',
-        'collective.z3cform.select2',
-        'collective.registration',
-        'collective.editablemenu',
-        'iaweb.mosaic',
+        "plone.api>=1.8.3",
+        "Products.GenericSetup>=1.8.2",
+        "setuptools",
+        "z3c.jbot",
+        "eea.facetednavigation",
+        "collective.faceted.taxonomywidget",
+        "collective.taxonomy",
+        "plone.app.imagecropping",
+        "plone.app.mosaic",
+        "imioweb.theme",
+        "collective.behavior.banner",
+        "collective.behavior.gallery",
+        "collective.easyform",
+        "collective.preventactions",
+        "iaweb.mosaic",
     ],
     extras_require={
-        'test': [
-            'plone.app.testing',
+        "test": [
+            "plone.app.testing",
             # Plone KGS does not use this version, because it would break
             # Remove if your package shall be part of coredev.
             # plone_coredev tests as of 2016-04-01.
-            'plone.testing>=5.0.0',
-            'plone.app.contenttypes',
-            'plone.app.robotframework[debug]',
-        ],
+            "plone.testing>=5.0.0",
+            "plone.app.contenttypes",
+            "plone.app.robotframework[debug]",
+        ]
     },
     entry_points="""
     [z3c.autoinclude.plugin]

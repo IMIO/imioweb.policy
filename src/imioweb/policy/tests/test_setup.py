acec30cc9215c405288367539b98f4248a2198ef
Benoit Suttor <bsuttor@imio.be>
2020-01-21 09:14:56 +0100

    Use get_installer from Products.CMFPlone.utils if exists

--- src/imioweb/policy/tests/test_setup.py
+++ src/imioweb/policy/tests/test_setup.py
@@ -8,6 +8,12 @@ from imioweb.policy.testing import IMIOWEB_POLICY_INTEGRATION_TESTING  # noqa
 import unittest
 
 
+try:
+    from Products.CMFPlone.utils import get_installer
+except ImportError:
+    get_installer = None
+
+
 class TestSetup(unittest.TestCase):
     """Test that imioweb.policy is properly installed."""
 
@@ -15,22 +21,22 @@ class TestSetup(unittest.TestCase):
 
     def setUp(self):
         """Custom shared utility setup for tests."""
-        self.portal = self.layer['portal']
-        self.installer = api.portal.get_tool('portal_quickinstaller')
+        self.portal = self.layer["portal"]
+        if get_installer:
+            self.installer = get_installer(self.portal, self.layer["request"])
+        else:
+            self.installer = api.portal.get_tool("portal_quickinstaller")
 
     def test_product_installed(self):
         """Test if imioweb.policy is installed."""
-        self.assertTrue(self.installer.isProductInstalled(
-            'imioweb.policy'))
+        self.assertTrue(self.installer.isProductInstalled("imioweb.policy"))
 
     def test_browserlayer(self):
         """Test that IImiowebPolicyLayer is registered."""
-        from imioweb.policy.interfaces import (
-            IImiowebPolicyLayer)
+        from imioweb.policy.interfaces import IImiowebPolicyLayer
         from plone.browserlayer import utils
-        self.assertIn(
-            IImiowebPolicyLayer,
-            utils.registered_layers())
+
+        self.assertIn(IImiowebPolicyLayer, utils.registered_layers())
 
 
 class TestUninstall(unittest.TestCase):
@@ -38,23 +44,23 @@ class TestUninstall(unittest.TestCase):
     layer = IMIOWEB_POLICY_INTEGRATION_TESTING
 
     def setUp(self):
-        self.portal = self.layer['portal']
-        self.installer = api.portal.get_tool('portal_quickinstaller')
+        self.portal = self.layer["portal"]
+        if get_installer:
+            self.installer = get_installer(self.portal, self.layer["request"])
+        else:
+            self.installer = api.portal.get_tool("portal_quickinstaller")
         roles_before = api.user.get_roles(TEST_USER_ID)
-        setRoles(self.portal, TEST_USER_ID, ['Manager'])
-        self.installer.uninstallProducts(['imioweb.policy'])
+        setRoles(self.portal, TEST_USER_ID, ["Manager"])
+        self.installer.uninstallProducts(["imioweb.policy"])
         setRoles(self.portal, TEST_USER_ID, roles_before)
 
     def test_product_uninstalled(self):
         """Test if imioweb.policy is cleanly uninstalled."""
-        self.assertFalse(self.installer.isProductInstalled(
-            'imioweb.policy'))
+        self.assertFalse(self.installer.isProductInstalled("imioweb.policy"))
 
     def test_browserlayer_removed(self):
         """Test that IImiowebPolicyLayer is removed."""
-        from imioweb.policy.interfaces import \
-            IImiowebPolicyLayer
+        from imioweb.policy.interfaces import IImiowebPolicyLayer
         from plone.browserlayer import utils
-        self.assertNotIn(
-            IImiowebPolicyLayer,
-            utils.registered_layers())
+
+        self.assertNotIn(IImiowebPolicyLayer, utils.registered_layers())
